1a2ade4a229ee8230ef23ef38591551258d4527f
Alireza Yavari <alireza@fanap.plus>
2019-05-28 17:00:11 +0430

    Add mattermost config

---
+++ src/MattermostBot/mattermost_config.py
@@ -0,0 +1,5 @@
+SSL_VERIFY = True  # Whether to perform SSL cert verification
+BOT_URL = 'https://fanype.fanap.plus/api/v4'  # with 'http://' and with '/api/v3' path. without trailing slash. '/api/v1' - for version < 3.0
+BOT_LOGIN = 'kia@fanap.plus'
+BOT_PASSWORD = '123456'
+BOT_TEAM = 'FanapPlus'  # possible in lowercase
