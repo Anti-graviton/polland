f54ab82227aeda33890f00b51a6739ec592043c9
Shadi Mahmoodian <shadimahm@gmail.com>
2019-05-28 16:59:27 +0430

    added mattermost conf

---
+++ src/MattermostBot/mattermost_config.py
@@ -0,0 +1,6 @@
+SSL_VERIFY = True  # Whether to perform SSL cert verification
+# with 'http://' and with '/api/v3' path. without trailing slash. '/api/v1' - for version < 3.0
+BOT_URL = 'http://fanype.fanap.plus/api/v4'
+BOT_LOGIN = 'shadi@fanap.plus'
+BOT_PASSWORD = '******'
+BOT_TEAM = 'FanapPlus'  # possible in lowercase
