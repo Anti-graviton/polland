b61acf0a4d34ff0b2a79b0ec8d1832839a761ff3
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-06-02 01:10:54 +0430

    Add question and choice models

--- src/web/models.py
+++ src/web/models.py
@@ -1,3 +1,35 @@
+from enum import Enum, unique
+
 from django.db import models
 
-# Create your models here.
+
+@unique
+class QuestionType(Enum):
+    MultipleChoiceSingleAnswer = 1,
+    MultipleChoiceMultipleAnswer = 2,
+    Written = 3,
+    Other = 4
+
+
+@unique
+class ChoiceType(Enum):
+    Selectable = 1,
+    Fillable = 2
+
+
+class Question(models.Model):
+    created_date = models.DateTimeField(auto_now=True)
+    publisher = models.CharField(max_length=200)
+    publish_date = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=False)
+    question_type = models.CharField(max_length=1)
+    category = models.TextField()
+    title = models.CharField(max_length=500)
+    body = models.TextField(max_length=2000)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_type = models.IntegerField(default=1)
+    value = models.TextField()
+    weight = models.IntegerField(default=1)
