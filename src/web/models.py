c80580a1377cdea64865726f107dd12c20cfe0d9
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-06-02 03:33:11 +0430

    Update models.py

--- src/web/models.py
+++ src/web/models.py
@@ -21,15 +21,16 @@ class Question(models.Model):
     created_date = models.DateTimeField(auto_now=True)
     publisher = models.CharField(max_length=200)
     publish_date = models.DateTimeField(auto_now=True)
-    is_active = models.BooleanField(default=False)
-    question_type = models.CharField(max_length=1)
-    category = models.TextField()
+    is_active = models.BooleanField(default=True)
+    question_type = models.CharField(max_length=1,
+                                     default=QuestionType.MultipleChoiceSingleAnswer)
+    category = models.TextField(default='general')
     title = models.CharField(max_length=500)
     body = models.TextField(max_length=2000)
 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_type = models.IntegerField(default=1)
+    choice_type = models.IntegerField(default=ChoiceType.Selectable)
     value = models.TextField()
     weight = models.IntegerField(default=1)
