c58db17148a845d15e26a5a5397759817703a910
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-06-04 03:20:53 +0430

    Create mmpy_bot_settings.py

---
+++ src/mattermost_bot/mmpy_bot_settings.py
@@ -0,0 +1,10 @@
+SSL_VERIFY = False  # Whether to perform SSL cert verification
+BOT_URL = 'https://<ADDRESS>/api/v4'
+BOT_LOGIN = '<LOGIN>'
+BOT_PASSWORD = '<PASSWORD>'
+BOT_TOKEN = None
+BOT_TEAM = '<TEAM>'  # possible in lowercase
+DEBUG = True
+PLUGINS = [
+    'plugins'
+]
