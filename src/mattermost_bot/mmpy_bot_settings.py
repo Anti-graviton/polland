f91c5b247a6ded66cab9a8dc880cc76a27dc8eae
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-06-05 11:24:17 +0430

    Update git-tracked bot config file

--- src/mattermost_bot/mmpy_bot_settings.py
+++ src/mattermost_bot/mmpy_bot_settings.py
@@ -1,9 +1,4 @@
-SSL_VERIFY = False  # Whether to perform SSL cert verification
-BOT_URL = 'https://<ADDRESS>/api/v4'
-BOT_LOGIN = '<LOGIN>'
-BOT_PASSWORD = '<PASSWORD>'
-BOT_TOKEN = None
-BOT_TEAM = '<TEAM>'  # possible in lowercase
+SSL_VERIFY = True
 DEBUG = True
 PLUGINS = [
     'plugins'
