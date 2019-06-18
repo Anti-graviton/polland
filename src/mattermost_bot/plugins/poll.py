8da5d1410b75e534fa066974ff6cf828dd7c2fdb
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-06-18 20:26:38 +0430

    Implement question list message handler

--- src/mattermost_bot/plugins/poll.py
+++ src/mattermost_bot/plugins/poll.py
@@ -1,16 +1,29 @@
 import re
+import os
 
 from mmpy_bot.bot import respond_to
 from mmpy_bot.dispatcher import Message
 
-from web.models import Question
+from web.models import Question, Choice, ChoiceType
 
 
 @respond_to('list', re.IGNORECASE)
 def get_question_list(msg: Message):
-    all_questions = Question.objects.all()
+    all_questions = Question.objects.order_by(
+        '-publish_date').all().prefetch_related('choices')
+
     if Question.objects.count() == 0:
         msg.reply_thread(
             'Sorry, there are currently no questions in the database.')
     else:
-        msg.reply(all_questions)
+        for idx, q in enumerate(all_questions):
+            question_str = "{}. {}".format(idx+1, q.title)
+            question_str += os.linesep
+            choices = [c for c in q.choices.values()]
+            for cidx, c in enumerate(choices):
+                question_str += "{}/{}. {}{}".format(
+                    idx+1, cidx+1, c['value'], os.linesep)
+            question_str += os.linesep
+            question_str += "---"
+
+            msg.reply(question_str)
