7633b2c3ac7ef9293d1575fe22ad64684ca773b2
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-06-21 00:15:33 +0430

    Delete ping_pong.py

--- src/mattermost_bot/plugins/ping_pong.py
+++
@@ -1,9 +0,0 @@
-import re
-
-from mmpy_bot.bot import respond_to
-from mmpy_bot.dispatcher import Message
-
-
-@respond_to('ping', re.IGNORECASE)
-def ping_pong(message: Message):
-    message.reply('PONG!')
