83f2d6c3d9b140314ce8f0d6773ae3ef496fc02b
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-06-21 18:18:41 +0430

    Create mattermost_bot.py

---
+++ src/bot/mattermost_bot.py
@@ -0,0 +1,35 @@
+import logging
+import sys
+from datetime import datetime
+
+from mmpy_bot.bot import Bot, settings, MessageDispatcher
+
+
+class PollandBot(Bot):
+
+    def __init__(self):
+        logging.basicConfig(**{
+            'format': '[%(asctime)s] %(message)s',
+            'datefmt': '%m/%d/%Y %H:%M:%S',
+            'level': logging.DEBUG,
+            'stream': sys.stdout,
+        })
+
+        settings.DEFAULT_REPLY = '''
+# نمی‌فهمم چی می‌گی!
+من یه ربات دون‌پایه‌ام! لطفا عین همین شیوه‌ای که این پایین نوشته با من صحبت کن.
+
+
+|command|معنی|
+|------|-----------------|
+|list ‌ |‌دریافت لیست همه‌ی نظر‌سنجی‌های فعال|
+
+
+'''
+
+        self.bot: Bot = Bot()
+        self.bot._plugins.plugins.append('bot.plugins')
+
+    def start(self):
+        self.bot.run()
+        print('Polland bot started at: {}'.format(datetime.now()))
