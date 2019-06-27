72cb3374157d75d9890c97e85b95dc9b64e8949c
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-06-27 17:22:28 +0430

    Simplify models

--- src/web/models.py
+++ src/web/models.py
@@ -1,20 +1,9 @@
-from enum import Enum, unique
-
 from django.db import models
 
 
-@unique
-class ChoiceType(Enum):
-    Selectable = 1
-    Fillable = 2
-
-
 class Question(models.Model):
-    created_date = models.DateTimeField(auto_now=True)
-    publisher = models.CharField(max_length=200)
-    publish_date = models.DateTimeField(auto_now=True)
-    is_active = models.BooleanField(default=True)
-    category = models.TextField(default='general')
+    creation_date = models.DateTimeField(auto_now=True)
+    creator = models.CharField(max_length=200)
     title = models.CharField(max_length=500)
     body = models.TextField(max_length=2000)
 
@@ -23,11 +12,7 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
-    choice_type = models.IntegerField(default=1,
-                                      choices=[(member.value, name) for name, member in ChoiceType.__members__.items()])
-    value = models.TextField()
-    weight = models.IntegerField(default=1)
-
+    value = models.TextField(max_length=500)
     question = models.ForeignKey(to=Question,
                                  on_delete=models.CASCADE,
                                  related_name='choices')
