b7832364d9e5b2ba945bd618576764d70825d85c
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-06-28 16:05:45 +0430

    Lint models.py

--- src/web/models.py
+++ src/web/models.py
@@ -1,21 +1,25 @@
+"""
+Import models from django db package
+"""
 from django.db import models
 
 
 class Question(models.Model):
+    """
+    Question model
+    """
     creation_date = models.DateTimeField(auto_now=True)
     creator = models.CharField(max_length=200)
     title = models.CharField(max_length=500)
     body = models.TextField(max_length=2000)
 
-    def __str__(self):
-        return "[{}]: {}".format(self.title, self.body)
 
 
 class Choice(models.Model):
+    """
+    Choice model
+    """
     value = models.TextField(max_length=500)
     question = models.ForeignKey(to=Question,
                                  on_delete=models.CASCADE,
                                  related_name='choices')
-
-    def __str__(self):
-        return self.value
