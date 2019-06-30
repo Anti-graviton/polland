058d39d8e4268bc84faa00481b7de58de066ae0e
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-06-30 17:52:46 +0430

    Add AserAnswer to admin panel

--- src/web/admin.py
+++ src/web/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Question, Choice
+from .models import Question, Choice, UserAnswer
 
 admin.site.register(Question)
 admin.site.register(Choice)
+admin.site.register(UserAnswer)
 # Register your models here.
